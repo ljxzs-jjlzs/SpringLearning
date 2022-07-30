4b58090b9858b85fab250be5ecb7ed16e5886dd6
jxLiu <2581545272@qq.com>
2022-07-30 10:54:46 +0800

    完善bilibili全部评论爬取函数

--- BiliBiliSpider_Comment.py
+++ BiliBiliSpider_Comment.py
@@ -24,6 +24,35 @@ class BiliBiliSpiderComment:
         with open('./BiliBili/BiliBiliBefore.json', 'r', encoding='utf-8') as f:
             self.data_before = json.loads(f.read())
 
+    def get_comments_all(self):
+        driver = webdriver.Chrome()
+        driver.maximize_window()
+        self.load_data()
+
+        for data in self.datas:
+            link = data['link']
+            driver.get(link)
+            time.sleep(5)
+            i = 0
+            while i < 4:
+                driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+                i += 1
+                time.sleep(5)
+
+            results = driver.find_elements(By.CSS_SELECTOR, '.reply-item div[class="content-warp"]')
+            i = 1
+            targets = []
+            for result in results:
+                target = {'index': i, 'text': result.find_element(By.CSS_SELECTOR, '.root-reply span').text}
+                i += 1
+                targets.append(target)
+            filename = self.filename_new + str(data['index_s']) + '.json'
+            with open(filename, 'w', encoding='utf-8') as f:
+                f.write(json.dumps(targets, indent=2, ensure_ascii=False))
+            print('新热搜', data['index_s'], '已经加载完毕')
+
+        driver.close()
+
     def get_comments(self):
         driver = webdriver.Chrome()
         driver.maximize_window()
