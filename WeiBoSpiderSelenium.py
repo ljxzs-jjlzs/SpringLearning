4e793c9e84dfd13761e1fa37f3cd0903eea93bfb
jxLiu <2581545272@qq.com>
2022-07-31 08:44:32 +0800

    将微博评论爬取功能与热搜爬取功能封装在一起，并未进行测试

--- WeiBoSpiderSelenium.py
+++ WeiBoSpiderSelenium.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import json
 import time
+import pyautogui
 
 
 class WeiBoSpider:
@@ -22,9 +23,17 @@ class WeiBoSpider:
         if not os.path.exists("./WeiBo"):
             # print("已经建立文件夹")
             os.mkdir('./WeiBo')
+        if not os.path.exists("./WeiBoComment"):
+            # print("已经建立文件夹")
+            os.mkdir('./WeiBoComment')
+        if not os.path.exists("./WeiBoCommentNew"):
+            # print("已经建立文件夹")
+            os.mkdir('./WeiBoCommentNew')
         self.filename = './WeiBo/WeiBo' + self.today + '.json'
         self.filename_ = './WeiBo/WeiBo' + str(build_time.strftime("%Y_%m_%d_%H")) + '.json'
         self.build_time = build_time
+        self.driver = None
+        self.before = None
 
     def get_page(self):
         driver = webdriver.Chrome()
@@ -128,7 +137,93 @@ class WeiBoSpider:
             print("Error")
         db.close()
 
-    def main(self):
+    def open(self):
+        self.driver = webdriver.Chrome()
+        self.driver.maximize_window()
+
+    def close(self):
+        self.driver.quit()
+
+    def login(self):
+        self.driver.get(
+            "https://s.weibo.com/weibo?q=%23%E5%90%B4%E5%95%8A%E8%90%8D%E5%8F%97%E5%AE%A1%E7%94%BB%E9%9D%A2%E5%85%AC"
+            "%E5%B8%83%23&Refer=top")
+        time.sleep(6)
+        temp = self.driver.find_element(By.CSS_SELECTOR, '.card-act')
+        temp = temp.find_elements(By.CSS_SELECTOR, 'li')[1]
+        temp = temp.find_element(By.CSS_SELECTOR, 'a')
+        temp.click()
+        pyautogui.moveTo(947, 695, duration=3)
+        pyautogui.click()
+        pyautogui.moveTo(872, 399, duration=3)
+        pyautogui.click()
+        time.sleep(2)
+        windows = self.driver.window_handles
+        self.driver.switch_to.window(windows[-1])
+        time.sleep(5)
+
+    def get_page_comment(self, base_url, index):
+        i = 1
+        j = 1
+        targets = []
+        base_url = base_url + '&page='
+        while i < 3:
+            url = base_url + str(i)
+            i += 1
+            self.driver.get(url)
+            time.sleep(5)
+            items = self.driver.find_elements(By.CSS_SELECTOR, '.card-wrap div[class="card"]')
+            if not items:
+                print('Error!Error!', 'index', index, '没有爬取成功！')
+                break
+            for item in items:
+                temp_item = item
+                try:
+                    item = item.find_element(By.CSS_SELECTOR, '.card-feed div[node-type="like"]')
+                except:
+                    continue
+                if j == 1:
+                    try:
+                        button = item.find_element(By.CSS_SELECTOR, 'a[action-type="fl_unfold"]')
+                        button_ = button.find_element(By.CSS_SELECTOR, '.wbicon')
+                        button_.click()
+                        time.sleep(2)
+                        content = item.find_element(By.CSS_SELECTOR, 'p[node-type="feed_list_content_full"]').text
+                    except:
+                        content = item.find_element(By.CSS_SELECTOR, '.txt').text
+                else:
+                    content = item.find_element(By.CSS_SELECTOR, '.txt').text
+                if j == 1:
+                    targets.append({'index': j, 'content': content})
+                    j += 1
+                elif len(content) < 50:
+                    targets.append({'index': j, 'content': content})
+                    j += 1
+                try:
+                    button_ = temp_item.find_elements(By.CSS_SELECTOR, '.card-act ul li')[1]
+                    button_ = button_.find_element(By.CSS_SELECTOR, 'a')
+                    button_.click()
+                    time.sleep(2)
+                except:
+                    i -= 1
+                    continue
+                comments = temp_item.find_elements(By.CSS_SELECTOR,
+                                                   'div[node-type="feed_list_repeat"] .card-review .txt')
+                for comment in comments:
+                    text = comment.text
+                    position = text.index('：')
+                    text = text[position + 1:]
+                    if text == '' or text == '图片评论 评论配图':
+                        continue
+                    targets.append({'index': j, 'content': text})
+                    j += 1
+
+        filename_ = './WeiBoComment/WeiBoComment' + str(index) + '.json'
+        with open(filename_, 'w', encoding='utf-8') as f:
+            f.write(json.dumps(targets, indent=2, ensure_ascii=False))
+        print(index, '页成功爬取!')
+
+    def main(self):  # 不更新before
         self.get_page()
         while len(self.result) < 50:
             print(len(self.result))
@@ -136,9 +231,55 @@ class WeiBoSpider:
         self.sava_json()
         self.save_database()
 
+    def main_all(self):  # 更新before
+        self.main()
+        self.open()
+        self.login()
+        filename = './WeiBo/WeiBo' + str(datetime.now().strftime("%d_%b_%Y")) + '.json'
+        with open(filename, 'r', encoding='utf-8') as f:
+            datas = json.loads(f.read())
+        for data in datas:
+            self.get_page_comment(data['links'], data['index_s'])
+
+        with open('./WeiBo/before.json', 'w', encoding='utf-8') as f1:
+            f1.write(json.dumps(datas, indent=2, ensure_ascii=False))
+        self.close()
+
+    def found(self, links: str):
+        for data in self.before:
+            if data['links'] == links:
+                return data['index_s']
+
+        return -1
+
+    def main_part(self):  # 更新before
+        self.main()
+        self.open()
+        self.login()
+        filename = './WeiBo/WeiBo' + str(datetime.now().strftime("%d_%b_%Y")) + '.json'
+        with open(filename, 'r', encoding='utf-8') as f:
+            datas = json.loads(f.read())
+        with open('./WeiBo/before.json', 'r', encoding='utf-8') as f:
+            self.before = json.loads(f.read())
+
+        for data in datas:
+            answer = self.found(data['links'])
+            if answer >= 0:
+                old_filename = './WeiBoComment/WeiBoComment' + str(answer) + '.json'
+                new_filename = './WeiBoCommentNew/WeiBoComment' + str(data['index_s']) + 'json'
+                with open(old_filename, 'r', encoding='utf-8') as f1, open(new_filename, 'w', encoding='utf-8') as f2:
+                    f2.write(json.dumps(json.loads(f.read()), indent=2, ensure_ascii=False))
+                continue
+
+            self.get_page_comment(data['links'], data['index_s'])
+
+        with open('./WeiBo/before.json', 'w', encoding='utf-8') as f1:
+            f1.write(json.dumps(datas, indent=2, ensure_ascii=False))
+
+        self.close()
+
 
 if __name__ == '__main__':
-
     now = datetime.now()
     weibo = WeiBoSpider(now)
     weibo.main()
